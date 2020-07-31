9092e04c5d2ad5520619e178de4771d15b995879
Hiroto Kitamura <kirikonknow@gmail.com>
2020-07-31 21:51:23 +0900

    add and apply ktlint

--- src/main/kotlin/com/github/eno314/spring/training/SpringTrainingApplication.kt
+++ src/main/kotlin/com/github/eno314/spring/training/SpringTrainingApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class SpringTrainingApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringTrainingApplication>(*args)
+    runApplication<SpringTrainingApplication>(*args)
 }
