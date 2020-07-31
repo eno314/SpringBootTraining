12dd5e86ff8ae5cdb45fbd046a3cbb5c05313577
Hiroto Kitamura <kirikonknow@gmail.com>
2020-07-31 21:30:54 +0900

    create project by spring initializr

---
+++ src/main/kotlin/com/github/eno314/spring/training/SpringTrainingApplication.kt
@@ -0,0 +1,11 @@
+package com.github.eno314.spring.training
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringTrainingApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringTrainingApplication>(*args)
+}
