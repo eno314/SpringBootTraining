dc79bc2bf3852dd7907e8f619aa44e6686184be9
Hiroto Kitamura <kirikonknow@gmail.com>
2020-08-28 01:51:39 +0900

    add swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.github.pozo:mapstruct-kotlin:1.3.1.2")
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
